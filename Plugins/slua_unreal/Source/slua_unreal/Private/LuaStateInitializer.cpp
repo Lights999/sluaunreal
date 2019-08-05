c9075852703838ea4ec1a4150146fd8070b64aab
zhangyang <zhangyang@q1.com>
2019-08-05 18:10:03 +0800

    add class

---
+++ Plugins/slua_unreal/Source/slua_unreal/Private/LuaStateInitializer.cpp
@@ -0,0 +1,86 @@
+﻿#include "LuaStateInitializer.h"
+
+namespace NS_SLUA
+{
+    // read file content
+    static uint8* ReadFile(IPlatformFile& PlatformFile, FString path, uint32& len) {
+        IFileHandle* FileHandle = PlatformFile.OpenRead(*path);
+        if (FileHandle) {
+            len = (uint32)FileHandle->Size();
+            uint8* buf = new uint8[len];
+
+            FileHandle->Read(buf, len);
+
+            // Close the file again
+            delete FileHandle;
+
+            return buf;
+        }
+
+        return nullptr;
+    }
+
+    static int32 PrintLog(NS_SLUA::lua_State *L)
+    {
+        FString str;
+        size_t len;
+        const char* s = luaL_tolstring(L, 1, &len);
+        if (s) str += UTF8_TO_TCHAR(s);
+        NS_SLUA::Log::Log("PrintLog %s", TCHAR_TO_UTF8(*str));
+        return 0;
+    }
+
+    LuaStateInitializer::LuaStateInitializer(const char* name) :state(name)
+    {
+    }
+
+    void LuaStateInitializer::Init()
+    {
+        state.onInitEvent.AddLambda([this]()
+        {
+            NS_SLUA::lua_State *L = state.getLuaState();
+            lua_pushcfunction(L, PrintLog);
+            lua_setglobal(L, "PrintLog");
+        });
+        //state.onInitEvent.AddUObject(this, &LuaStateInitializer::LuaStateInitCallback);
+        state.init();
+
+        state.setLoadFileDelegate([](const char* fn, uint32& len, FString& filepath)->uint8* {
+
+            IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+            //TODO:
+            FString path = FPaths::ProjectContentDir();
+            FString filename = UTF8_TO_TCHAR(fn);
+            FString fRootName = UTF8_TO_TCHAR("Lua");//TODO: 从资源管理器获取Lua的根目录 zhang-yang
+            path /= fRootName;
+            path /= filename.Replace(TEXT("."), TEXT("/"));
+
+            TArray<FString> luaExts = { UTF8_TO_TCHAR(".lua"), UTF8_TO_TCHAR(".luac") };
+            for (auto& it : luaExts) {
+                auto fullPath = path + *it;
+                auto buf = ReadFile(PlatformFile, fullPath, len);
+                if (buf) {
+                    fullPath = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*fullPath);
+                    filepath = fullPath;
+                    return buf;
+                }
+            }
+
+            return nullptr;
+        });
+    }
+
+    void LuaStateInitializer::Release()
+    {
+        state.close();
+    }
+
+    //void LuaStateInitializer::LuaStateInitCallback()
+    //{
+    //    NS_SLUA::lua_State *L = state.getLuaState();
+    //    lua_pushcfunction(L, PrintLog);
+    //    lua_setglobal(L, "PrintLog");
+    //}
+}
+
+
---
+++ Plugins/slua_unreal/Source/slua_unreal/Public/LuaStateInitializer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "LuaState.h"
+
+namespace NS_SLUA
+{
+    class SLUA_UNREAL_API LuaStateInitializer
+    {
+
+    public:
+        LuaStateInitializer(const char* name = nullptr);
+ /*       virtual ~LuaStateInitializer();*/
+
+        void Init();
+        void Release();
+
+    private:
+        UFUNCTION()
+        void LuaStateInitCallback();
+
+        // create global state, freed on app exit
+        NS_SLUA::LuaState state;
+    };
+}
